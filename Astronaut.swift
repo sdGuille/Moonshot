f5b5bb7144270b604bc7a95499d327312091dc66
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-10-06 07:43:09 -0600

    Create the business logic from astronauts.json

---
+++ Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Guillermo Ruiz Baires on 6/10/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
