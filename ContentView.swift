06b7babbd24d421f668f5f39ce6eeec24fd58f24
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-10-06 07:47:07 -0600

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -7,28 +7,13 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
 
 struct ContentView: View {
-    let layout = [
-        GridItem(.adaptive(minimum: 80)),
-    ]
-    
+    let astronauts = Bundle.main.decode("astronauts.json")
+
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(1..<1000) {
-                    Text("Item \($0)")
-                }
-            }
+        VStack {
+            Text("\(astronauts.count)")
         }
     }
 }
