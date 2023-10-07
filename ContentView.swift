c3ec6db7c75f5e00f458475aa074fe9a3953c547
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-10-07 08:04:45 -0600

    Showing the list of missions

--- ContentView.swift
+++ ContentView.swift
@@ -9,12 +9,54 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let mission: [Mission] = Bundle.main.decode("missions.json")
 
     var body: some View {
-        VStack {
-            Text("\(astronauts.count)")
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(mission) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
     }
 }
